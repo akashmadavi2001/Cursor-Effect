576f2dc164bb1e81bee3087cf4518b964e5a7835
Akash Madavi <akashmadavi2001@gmail.com>
2025-06-16 08:33:24 +0530

    script.js

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ let smoke = document.querySelector('.smoke');
 
 document.addEventListener('mousemove',(e)=>{
           let elem = document.createElement('div');
-    elem.classList.add('element');
+          elem.classList.add('element');
 
-    elem.style.left = `${e.clientX}px`;
-    elem.style.top = `${e.clientY}px`;
-    smoke.appendChild(elem);    
+          elem.style.left = `${e.clientX}px`;
+          elem.style.top = `${e.clientY}px`;
+          
+          smoke.appendChild(elem);    
 
     elem.addEventListener('animationend',()=>{
-        elem.remove();
-    })
-  })
+          elem.remove();
+    });
+  });
